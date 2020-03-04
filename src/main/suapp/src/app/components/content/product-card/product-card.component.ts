39fed55df48c0b185119491a1a84dce18e4e4d32
David C <davidcastellanos101@gmail.com>
2020-03-03 20:45:05 -0500

    fix css

--- src/main/suapp/src/app/components/content/product-card/product-card.component.ts
+++ src/main/suapp/src/app/components/content/product-card/product-card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from "@angular/core";
 @Component({
   selector: "app-product-card",
   templateUrl: "./product-card.component.html",
-  styleUrls: ["./product-card.component.scss", "./aux.css"]
+  styleUrls: ["./product-card.component.scss", "./product-card-2.css"]
 })
 export class ProductCardComponent implements OnInit {
 
