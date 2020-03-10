43a1a8e7a879566d430f96ee059bb72c0aaf89a2
David C <davidcastellanos101@gmail.com>
2020-03-10 16:34:31 -0500

    página categorías

--- src/main/suapp/src/assets/lib/custom/js/carousel.js
+++ src/main/suapp/src/assets/lib/custom/js/carousel.js
@@ -1,18 +1,3 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-categorias',
-  templateUrl: './categorias.component.html',
-  styleUrls: ['./categorias.component.scss']
-})
-export class CategoriasComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
 /** 
 (function(){
   // setup your carousels as you normally would using JS
